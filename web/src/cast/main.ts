477f73a6fb1436452182e6d671d964e2abb4dcf5
Creighton Linza <creightonlinza@gmail.com>
2026-06-30 10:56:55 -0400

    cleanup

--- web/src/cast/main.ts
+++ web/src/cast/main.ts
@@ -192,7 +192,7 @@ function isValidJobId(value: string) {
 
 function stripTrailingSlashes(value: string) {
   let end = value.length;
-  while (end > 0 && value.charCodeAt(end - 1) === 47) {
+  while (end > 0 && value[end - 1] === "/") {
     end -= 1;
   }
   return value.slice(0, end);
